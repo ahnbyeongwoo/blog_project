4bfe3a5ca42f7ad1b93f4e11f41f9a9d7e371c38
ahnbyeongwoo <bw5871@gmail.com>
2025-06-01 17:39:19 +0900

    delete axios

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { Pool } = require("pg");
-const axios = require("axios");
+
 
 dotenv.config();
 
