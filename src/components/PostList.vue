537707e47f0bab807f5a3f2264438046e6178ad2
ahnbyeongwoo <bw5871@gmail.com>
2025-06-06 17:28:08 +0900

    a

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -140,6 +140,7 @@ export default {
 
 .list-header {
   /* position: fixed;  */
+  height: 100vh;
   top: 0; /* 상단 고정 시 기준점 */
   left: 0; /* 좌측 정렬 기준 */
   right: 0; /* 우측 정렬 기준 */
@@ -166,7 +167,6 @@ export default {
 
 main {
   height: 100vh;
-  
   /* padding-top: 84px; */
   /* 헤더가 fixed로 고정되어 있기 때문에, 겹치지 않도록 헤더 높이(64px)보다 약간 큰 여백 확보 */
   max-width: 800px;
