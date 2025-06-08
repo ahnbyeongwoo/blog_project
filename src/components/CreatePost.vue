27f8d9f35739afd4661e3247abfa8db42075c7f9
ahnbyeongwoo <bw5871@gmail.com>
2025-06-08 16:17:35 +0900

    aaa

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -131,9 +131,9 @@ export default {
 }
 .logout-btn {
   margin-right: 56px;
-  border: none;
   background-color: #234567;
   color: #fff;
+  border: none;
   font-size: 15px;
   font-weight: 600;
   cursor: pointer;
@@ -152,7 +152,7 @@ main {
   background: #fff;
   max-width: 1000px;
   width: 100%;
-  margin: 16px auto 0 auto; /* 위쪽 여백 줄임 */
+  margin: 24px auto 0 auto; /* 위쪽 여백 줄임 */
   border-radius: 18px;
   box-shadow: 0 2px 16px rgba(60,80,100,0.09);
   padding: 40px 36px 32px 36px;
